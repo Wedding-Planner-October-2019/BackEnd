102e3694b4c2216644c6d28052fb76fc4d8ceb1f
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-19 13:33:10 -0700

    server is running locally

---
+++ API/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const helmet = require("helmet");
+const server = express();
+const cors = require("cors");
+
+server.use(helmet(), express.json(), cors());
+
+//Deployment
+server.get("/", (req, res) => {
+  res.status(200).json("Successful Deployment");
+});
