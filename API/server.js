688256b8897d450ff9489f2ab42a693911aec71d
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-19 16:30:35 -0700

    adding route files

--- API/server.js
+++ API/server.js
@@ -6,8 +6,12 @@ const cors = require("cors");
 server.use(helmet(), express.json(), cors());
 
 //Imports
+// const userRoute = require('./Routes/Auth/user/userRoute');
+// const weddingRoute = require('./Routes/')
 
 //Routes
+// server.use('/auth/user', userRoute);
+// server.use('/auth/user/:id/weddings', weddingRoute);
 
 //Deployment
 server.get("/", (req, res) => {
