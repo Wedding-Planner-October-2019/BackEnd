931cfd962cf3a6e8c7c14689b28574a159f48429
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-20 10:54:46 -0700

    migrations and seeds ran

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,11 +1,11 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("users").then(function() {
+  return knex("user").then(function() {
     // Inserts seed entries
-    return knex("users").insert([
+    return knex("user").insert([
       { username: "weddingPlaner", password: "password1" },
-      { username: "weddingPlaner", password: "password1" },
-      { username: "weddingPlaner", password: "password1" }
+      { username: "weddingPlaner1", password: "password1" },
+      { username: "weddingPlaner2", password: "password1" }
     ]);
   });
 };
