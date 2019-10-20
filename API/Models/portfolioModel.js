34929dd011bfe3ac9474e257b2af5f562e92f329
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-19 20:55:46 -0700

    figuring out the last route

---
+++ API/Models/portfolioModel.js
@@ -0,0 +1,13 @@
+// const db = require("../../data/");
+
+// module.exports = {
+//   showProfile
+// };
+
+// function showProfile(id) {
+//   //use find ID to and then select only the user's name, contact info, and weddings.
+//   return db("users")
+//     .select("id", "name")
+//     .where({ id })
+//     .first();
+// }
