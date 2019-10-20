a5de1809a5a73bb3687afc185ce92a6f1c37e82a
Daniel Jones <daniel.jones2000@gmail.com>
2019-10-20 09:37:35 -0700

    seeds wip

--- data/seeds/03-weddings.js
+++ data/seeds/03-weddings.js
@@ -9,13 +9,42 @@ exports.seed = function(knex) {
           wedding_name: "Smith's Wedding",
           venue: "Old Winery",
           description:
-            "It was a traditional wedding held on a lovely summer evening. We were able to plan a wedding for over 200 guest. I kept the event underbudget"
+            "It was a traditional wedding held on a lovely summer evening. We were able to plan a wedding for over 200 guests. I kept the event underbudget",
+          user_info_id: 1
+        },
+        {
+          wedding_name: "Jennifer Cutler & Oscar Flores Wedding",
+          venue: "Smith's Farm",
+          description:
+            "It was a traditional wedding held on a lovely fall evening. We were able to plan a wedding for 50 guests.",
+          user_info_id: 2
+        },
+        {
+          wedding_name: "Jennifer Cutler & Oscar Flores Wedding",
+          venue: "Smith's Farm",
+          description:
+            "It was a traditional wedding held on a lovely summer evening. We were able to plan a wedding for over 200 guests. I kept the event underbudget",
+          user_info_id: 2
         },
         {
           wedding_name: "The 's Wedding",
           venue: "Old Winery",
           description:
-            "It was a traditional wedding held on a lovely summer evening. We were able to plan a wedding for over 200 guest. I kept the event underbudget"
+            "It was a traditional wedding held on a lovely summer evening. We were able to plan a wedding for over 200 guests. I kept the event underbudget",
+          user_info_id: 3
+        },
+        {
+          wedding_name: "The Gates's Wedding",
+          venue: "Space needle",
+          description: "A small wedding",
+          user_info_id: 3
+        },
+        {
+          wedding_name: "Hunter & Mackenzie Wedding",
+          venue: "East Sun Winery",
+          description:
+            "It was a nontraditional wedding held on a warm summer afternoon. We were able to plan a wedding for over 150 guests. The theme was formal country",
+          user_info_id: 3
         }
       ]);
     });
